74e59b6b0c0fd74e7f3d6fdc4eb7a061be5d3f84
Erika Bausyte <e.bausyte93@gmail.com>
2025-06-11 09:24:24 +0200

    changes

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,8 +1,7 @@
-export const Sum = ({a = 0 , b = 0}) =>{
-  return(
-
-    <p>Sum of {a} and {b} is {a + b }</p>
-  )
-
+export const Sum = ({ a = 0, b = 0 }) => {
+  return (
+    <p>
+      {`{Sum of ${a} and ${b} is ${a + b}`}
+    </p>
+  );
 };
-
